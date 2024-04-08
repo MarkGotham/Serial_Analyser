0beffd493735550375c7305fcdc6581e2b048752
Mark Gotham <mark.r.gotham@durham.ac.uk>
2024-04-08 08:18:29 +0100

    rotation and re-combination method reported in Hopper "The Music of David Lumsdaine", p.21

--- transformations.py
+++ transformations.py
@@ -185,6 +185,123 @@ def pairSwapKrenek(
 
 # ------------------------------------------------------------------------------
 
+def lumsdaine_4x(
+        row: Union[list, None] = None,
+) -> list[list]:
+    """
+    A multipart
+    rotation and re-combination
+    method as reported in
+    Hopper's "The Music of David Lumsdaine", p.21.
+
+    >>> for x in lumsdaine_4x():
+    ...     print(x)
+    [11, 6, 5, 0, 3, 2, 9, 8, 10, 4, 1, 7]
+    [11, 9, 6, 8, 5, 10, 0, 4, 3, 1, 2, 7]
+    [11, 10, 2, 8, 3, 9, 0, 7, 5, 1, 6, 4]
+    [11, 0, 10, 7, 2, 5, 8, 1, 3, 6, 9, 4]
+    [2, 6, 10, 1, 11, 5, 9, 7, 3, 0, 8, 4]
+    [2, 9, 6, 7, 10, 3, 1, 0, 11, 8, 5, 4]
+    [2, 3, 5, 7, 11, 9, 1, 4, 10, 8, 6, 0]
+    [2, 1, 3, 4, 5, 10, 7, 8, 11, 6, 9, 0]
+    [5, 6, 3, 8, 2, 10, 9, 4, 11, 1, 7, 0]
+    [5, 9, 6, 4, 3, 11, 8, 1, 2, 7, 10, 0]
+    [5, 11, 10, 4, 2, 9, 8, 0, 3, 7, 6, 1]
+    [5, 8, 11, 0, 10, 3, 4, 7, 2, 6, 9, 1]
+    [10, 6, 11, 7, 5, 3, 9, 0, 2, 8, 4, 1]
+    [10, 9, 6, 0, 11, 2, 7, 8, 5, 4, 3, 1]
+    [10, 2, 3, 0, 5, 9, 7, 1, 11, 4, 6, 8]
+    [10, 7, 2, 1, 3, 11, 0, 4, 5, 6, 9, 8]
+    [3, 6, 2, 4, 10, 11, 9, 1, 5, 7, 0, 8]
+    [3, 9, 6, 1, 2, 5, 4, 7, 10, 0, 11, 8]
+    [3, 5, 11, 1, 10, 9, 4, 8, 2, 0, 6, 7]
+    [3, 4, 5, 8, 11, 2, 1, 0, 10, 6, 9, 7]
+
+    """
+    if row is None:
+        row = [11, 6, 5, 0, 3, 2, 9, 8, 10, 4, 1, 7]
+    out = []
+    for i in range(5):  # run 4, update to new starting row, and run again
+        out += lumsdaine_4(row)
+        row = every_nth(out[-1], start_index=4, n=5)
+    return out
+
+
+def lumsdaine_4(
+        row1: list | None = None,
+) -> list[list]:
+    """
+    One phase of `lumsdaine_4x()`.
+
+    >>> for x in lumsdaine_4([11, 6, 5, 0, 3, 2, 9, 8, 10, 4, 1, 7]):
+    ...     print(x)
+    [11, 6, 5, 0, 3, 2, 9, 8, 10, 4, 1, 7]
+    [11, 9, 6, 8, 5, 10, 0, 4, 3, 1, 2, 7]
+    [11, 10, 2, 8, 3, 9, 0, 7, 5, 1, 6, 4]
+    [11, 0, 10, 7, 2, 5, 8, 1, 3, 6, 9, 4]
+
+    >>> for x in lumsdaine_4([5, 9, 7, 3, 0, 8, 4, 2, 6, 10, 1, 11]):
+    ...     print(x)
+    [5, 9, 7, 3, 0, 8, 4, 2, 6, 10, 1, 11]
+    [5, 4, 9, 2, 7, 6, 3, 10, 0, 1, 8, 11]
+    [5, 6, 8, 2, 0, 4, 3, 11, 7, 1, 9, 10]
+    [5, 3, 6, 11, 8, 7, 2, 1, 0, 9, 4, 10]
+
+    """
+    if row1 is None:
+        row1 = [11, 6, 5, 0, 3, 2, 9, 8, 10, 4, 1, 7]
+    row2 = l_hexachord_pairs(row1)
+    row3 = every_nth(row2)
+    row4 = l_hexachord_pairs(row3)
+    return [row1, row2, row3, row4]
+
+
+def l_hexachord_pairs(this_row: list):
+    """
+    Re-arrange 0, 6, 1, 7 etc.
+
+    >>> l_hexachord_pairs([11, 6, 5, 0, 3, 2, 9, 8, 10, 4, 1, 7])
+    [11, 9, 6, 8, 5, 10, 0, 4, 3, 1, 2, 7]
+    """
+    out_row = []
+    for i in range(6):
+        out_row += [this_row[i], this_row[i + 6]]
+    return out_row
+
+
+def every_nth(
+    row: list,
+    start_index: int = 0,
+    n: int = 5,
+    ran: range = range(12)
+) -> list:
+    """
+    Cycle through the row (mod 12) with a step size of n.
+
+    >>> row = [11, 9, 6, 8, 5, 10, 0, 4, 3, 1, 2, 7]
+    >>> every_nth(row)
+    [11, 10, 2, 8, 3, 9, 0, 7, 5, 1, 6, 4]
+
+    By default,
+    start at index 0 and iterate 12 times (0-12),
+    though both the start index and the range() are settable arguments.
+    Hence this equivlance:
+
+    >>> every_nth(row, ran=range(1, 13))
+    [10, 2, 8, 3, 9, 0, 7, 5, 1, 6, 4, 11]
+
+    >>> every_nth(row, start_index=5)
+    [10, 2, 8, 3, 9, 0, 7, 5, 1, 6, 4, 11]
+
+    """
+    out_row = []
+    for i in ran:
+        out_row.append(row[(start_index + i * n) % 12])
+    return out_row
+
+
+# ------------------------------------------------------------------------------
+
 class RowTester(unittest.TestCase):
 
     def testTranspose(self):
